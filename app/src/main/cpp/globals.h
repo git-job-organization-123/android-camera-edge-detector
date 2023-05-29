cca18c6dee381af3f8b3c4fb30e32f3dbf1c563b
git-for-jobs-123 <->
2023-05-29 14:02:54 +0300

    Create globals.h to store global variables

---
+++ app/src/main/cpp/globals.h
@@ -0,0 +1,19 @@
+enum PreviewMode {
+  DETECT_PREVIEW_EDGES_WHITE,
+  DETECT_PREVIEW_EDGES_RED,
+  DETECT_PREVIEW_EDGES_GREEN,
+  DETECT_PREVIEW_EDGES_BLUE,
+  DETECT_PREVIEW_EDGES_GRAYSCALE,
+  DETECT_PREVIEW_EDGES_WHITE_WITH_BACKGROUND,
+  DETECT_EDGES_FAST,
+  DETECT_EDGES_FAST_LINES,
+};
+
+PreviewMode previewMode;
+PreviewMode previousPreviewMode;
+
+GLuint ibo;
+GLuint vbo;
+
+GLfloat *vboData;
+GLushort *iboData;
